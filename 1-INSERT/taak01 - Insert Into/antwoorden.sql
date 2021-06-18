e2966573e48f164fe8bf9b87a8c38c3ab2562cb3
Samuel Adu <samuel.adu@student.rocva.nl>
2021-06-18 11:14:02 +0200

    insert into

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (5,'Piet', 'Pieters', '9A', 	'Albert Cuypstraat 5' ,	'1234AB', '2001-04-28',	'Ouderkerk')
+INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (6,'Johanna', 'Eduardo', '9C', 	'Pieter Aertzstraat 44' ,	'1073CD', '2001-09-02',	'Hoorn')
+INSERT INTO studenten (student_id, voornaam, achternaam , klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (7,'Angela', 'Mohammed', '9C', 	'Piet Mondriaanplein 14' ,	'1299CD', '2002-05-17',	'Amstelveen')
